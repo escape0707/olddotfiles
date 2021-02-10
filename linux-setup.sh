bea103f42b83985863298f8e540e64e12ebe7f93
escape0707 <tothesong@gmail.com>
2021-02-10 15:14:00 +0800

    Add aria2.conf for linux

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,5 +1,9 @@
 cd $(dirname $(readlink -f $0))/linux
 DotfilesFolder=$PWD
 
+mkdir ~/.aria2
+ln -s $DotfilesFolder/aria2.conf ~/.aria2/aria2.conf
+
 ln -s $DotfilesFolder/.zshrc ~/.zshrc
+
 ln -s $DotfilesFolder/pacman-pkglist.txt ~/pacman-pkglist.txt
